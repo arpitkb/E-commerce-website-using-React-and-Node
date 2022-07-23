6e8a7a3d884e96ee22fe5b89e7ed8d9e0aa74e87
ashish bansal <bansal.ashu35@gmail.com>
2022-07-24 02:06:53 +0530

    fixed bug

--- client/src/components/cart/CartItem.js
+++ client/src/components/cart/CartItem.js
@@ -8,6 +8,9 @@ const CartItem = ({ item, chngQty, remove }) => {
   // const addOne = () => {
   //   chngQty(item.product, parseInt(item.qty) + 1);
   // };
+
+  // extra code
+  
   // const removeOne = () => {
   //   if (item.qty === 1) return;
   //   chngQty(item.product, parseInt(item.qty) - 1);
