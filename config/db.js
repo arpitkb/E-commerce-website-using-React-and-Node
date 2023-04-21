625eb08b8917fbee9c8e160b2685acca3a3b5303
arpit <bansal_851937@student.nitw.ac.in>
2023-04-22 03:21:37 +0530

    bug fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async (db) => {
   try {
     await mongoose.connect(db);
-    console.log("Data base connected.....".cyan.inverse);
+    console.log("Data base connected.....");
   } catch (err) {
     console.log(err);
 
