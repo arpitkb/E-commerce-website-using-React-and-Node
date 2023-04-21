9d9c2e3072875999a326eb2b53eff33ea082b7c0
arpit <bansal_851937@student.nitw.ac.in>
2023-04-22 02:35:12 +0530

    bug fix

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 // creating an instance of axios
 const api = axios.create({
-    baseURL : 'https://shopzone-zkv4.onrender.com',
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-    }
+    baseURL : 'https://shopzone-zkv4.onrender.com'
 });
 
 
